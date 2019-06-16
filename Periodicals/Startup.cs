16f5afabf909289ff580060256432a21d18ff915
yevhen hordiienko <yevhen.hordiienko@nure.ua>
2019-06-16 20:00:29 +0300

    Createted by template with auth. Packages were Updated

---
+++ Periodicals/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Periodicals.Startup))]
+namespace Periodicals
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
